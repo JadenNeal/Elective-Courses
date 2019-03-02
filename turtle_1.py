ec18eab3ee6e1cc7a6d82e97218f791532fdd6d7
Jaden Neal <longweicai1997@163.com>
2019-03-02 19:50:03 +0800

    a test

--- turtle_1.py
+++ turtle_1.py
@@ -1,14 +1,16 @@
 from turtle import *
+speed(9)
 
 
+def drawBox(sidelength):
+    for i in range(4):
+        forward(sidelength)
+        left(90)
 
 
-title('EX1')
-forward(100)
-left(90)
-forward(100)
-left(90)
-forward(100)
-left(90)
-forward(100)
-left(90)
+for i in range(36):
+    pencolor('blue')
+    drawBox(150)
+    right(10)
+
+exitonclick()
